d7b450c1cd30c089b83c396e71e7f8592453af58
Andrianto Cahyono Putro <andri.c.putro@gmail.com>
2022-03-27 23:51:35 +0700

    add config

--- configs/config.local.go
+++ configs/config.local.go
@@ -2,6 +2,7 @@ package configs
 
 const (
 	WEB_PORT = "8888"
+	DB_HOST = "localhost"
 	DB_USER = "root"
 	DB_PASSWORD = ""
 	DB_NAME = "sigolang-sv"
--- database/db.go
+++ database/db.go
@@ -3,11 +3,10 @@ package database
 import (
 	"fmt"
 	"log"
+	"sigolang-sv/configs"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-
-	config "sigolang-sv/configs"
 )
 
 var (
@@ -16,9 +15,10 @@ var (
 )
 
 func MySQLConnect() {
-	dsn := fmt.Sprintf("%v:%v@/%v?charset=utf8&parseTime=True", config.DB_USER, config.DB_PASSWORD, config.DB_NAME)
+	dsn := fmt.Sprintf("%v:%v@/%v?charset=utf8&parseTime=True", configs.DB_USER, configs.DB_PASSWORD, configs.DB_NAME)
+	// dsn := ("andri:j26dlZaoZ8wQpxT8@tcp(127.0.1:3306)/andri?charset=utf8&parseTime=True&loc=Local")
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
+	
 	if err != nil {
 		log.Println("DB Connection Error: ")
 	} else {
